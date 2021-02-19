549396255386816c65fce16167024585f596da25
Pierpaolo Follia <pfollia@gmail.com>
2021-02-19 15:23:56 +0100

    Better state managemnet (#1)
    * Better connection error handling
    * Add opening_time config
    * Fix opening status

--- src/garage-door.ts
+++ src/garage-door.ts
@@ -1,12 +1,12 @@
 import {
   AccessoryPlugin,
-  Controller,
-  PlatformAccessory,
-  Service,
-  Logging,
+  APIEvent,
   CharacteristicEventTypes,
   CharacteristicSetCallback,
-  APIEvent,
+  Controller,
+  Logging,
+  PlatformAccessory,
+  Service,
 } from 'homebridge';
 import { API } from 'homebridge/lib/api';
 import { AccessoryConfig } from 'homebridge/lib/server';
@@ -14,6 +14,8 @@ import EventSource from 'eventsource';
 import { debounce } from 'lodash';
 import { withPrefix } from 'homebridge/lib/logger';
 import fetch from 'node-fetch';
+import Timeout = NodeJS.Timeout;
+import retry from 'async-retry';
 
 const DEBOUNCE_TIME = 500;
 
@@ -21,10 +23,10 @@ export function initESPHome(
   host: string,
   port = 80,
   logger: Logging = withPrefix('ESPHome'),
-  listener: (e: ESPHomeEvent) => void = _e => {}
+  stateListener: (state: ESPHomeEvent) => void = _state => {}
 ): EventSource {
   const eventSource = new EventSource(`http://${host}:${port}/events`);
-  const fn = listener ? debounce(e => listener(e), DEBOUNCE_TIME) : _e => {};
+  const fn = stateListener ? debounce(e => stateListener(e), DEBOUNCE_TIME) : _e => {};
 
   logger.info('Event source started at ' + `http://${host}:${port}/events`);
   eventSource.addEventListener('state', e => {
@@ -45,6 +47,7 @@ interface GarageDoorConfig extends AccessoryConfig {
   name: string;
   host: string;
   port: number;
+  opening_time: number;
 }
 
 interface ESPHomeEvent {
@@ -55,6 +58,7 @@ interface ESPHomeEvent {
 }
 
 const MAX_RETRY = 5;
+const DEFAULT_OPEN_TIME = 30; // 30 seconds
 
 export class GarageDoor implements AccessoryPlugin {
   private readonly accessory: PlatformAccessory;
@@ -62,25 +66,40 @@ export class GarageDoor implements AccessoryPlugin {
   espTemplateName: string;
   espTemplateId: string;
   private eventSource: EventSource;
+  private initialized: boolean;
+  private timeout: Timeout;
+  private aliveTimeout: Timeout;
 
   constructor(readonly logger: Logging, readonly config: GarageDoorConfig, readonly api: API) {
     this.accessory = new api.platformAccessory(config.name, api.hap.uuid.generate(config.name));
-    api.on(APIEvent.DID_FINISH_LAUNCHING, async () => {
-      for (let i = 0; i < MAX_RETRY; i++) {
-        try {
-          await this.connectToESPSource();
-        } catch (e) {
-          logger.error(e.toString(), e);
-        }
-      }
-    });
-    api.on(APIEvent.SHUTDOWN, () => {
-      try {
-        this.eventSource?.close();
-      } catch (e) {
-        logger.error(e);
-      }
-    });
+    this.initialized = false;
+    api.on(APIEvent.DID_FINISH_LAUNCHING, async () => await this.initConnection());
+    api.on(APIEvent.SHUTDOWN, () => this.closeConnection());
+  }
+
+  private closeConnection() {
+    try {
+      this.eventSource?.close();
+    } catch (e) {
+      this.logger.error(e);
+    } finally {
+      this.initialized = false;
+    }
+  }
+
+  private async initConnection(): Promise<EventSource> {
+    try {
+      return await retry(() => this.connectToESPSource(), {
+        retries: MAX_RETRY,
+        minTimeout: 5000,
+        maxTimeout: 5000,
+
+        onRetry: () => this.logger.info('Retrying connect to hardware'),
+      });
+    } catch (e) {
+      this.logger.info(`error: ${e.message}`, e);
+      return null;
+    }
   }
 
   getControllers(): Controller[] {
@@ -117,6 +136,13 @@ export class GarageDoor implements AccessoryPlugin {
         }
       );
 
+    this.service
+      .getCharacteristic(Characteristic.CurrentDoorState)
+      .updateValue(Characteristic.CurrentDoorState.CLOSED);
+    this.service
+      .getCharacteristic(Characteristic.TargetDoorState)
+      .updateValue(Characteristic.CurrentDoorState.CLOSED);
+
     const infoService =
       this.accessory.getService(this.api.hap.Service.AccessoryInformation) ||
       this.accessory.addService(this.api.hap.Service.AccessoryInformation);
@@ -131,7 +157,7 @@ export class GarageDoor implements AccessoryPlugin {
 
   identify(): void {}
 
-  private async handleSetState(state: string): Promise<boolean> {
+  private async handleSetState(state: 'open' | 'close'): Promise<boolean> {
     try {
       const resp = await fetch(
         `http://${this.config.host}:${this.config.port || 80}/${this.espTemplateName}/${
@@ -141,6 +167,35 @@ export class GarageDoor implements AccessoryPlugin {
           method: 'POST',
         }
       );
+      if (this.initialized) {
+        const Characteristic = this.api.hap.Characteristic;
+        const isOpening = state === 'open';
+        this.service.updateCharacteristic(
+          Characteristic.CurrentDoorState,
+          isOpening
+            ? Characteristic.CurrentDoorState.OPENING
+            : Characteristic.CurrentDoorState.CLOSING
+        );
+        if (this.timeout) {
+          clearTimeout(this.timeout);
+          this.timeout = null;
+        }
+        if (isOpening) {
+          const openingTime = this.config.opening_time || DEFAULT_OPEN_TIME;
+          this.timeout = setTimeout(() => {
+            this.logger.debug('Timeout triggered, update door state to open');
+            this.service.updateCharacteristic(
+              Characteristic.CurrentDoorState,
+              Characteristic.CurrentDoorState.OPEN
+            );
+            this.service.updateCharacteristic(
+              Characteristic.TargetDoorState,
+              Characteristic.TargetDoorState.OPEN
+            );
+            this.timeout = null;
+          }, openingTime * 1000);
+        }
+      }
       return resp.ok;
     } catch (e) {
       this.logger.error(e);
@@ -151,38 +206,53 @@ export class GarageDoor implements AccessoryPlugin {
   private handleDoorState(e: ESPHomeEvent) {
     const Characteristic = this.api.hap.Characteristic;
     [this.espTemplateName, this.espTemplateId] = e.id.split('-');
-    if (this.service) {
+    if (this.service && this.initialized) {
       this.logger.debug('GarageDoorOpener Service: updating door state', e);
-      this.service.updateCharacteristic(
-        Characteristic.CurrentDoorState,
-        e.state === 'CLOSED'
-          ? Characteristic.CurrentDoorState.CLOSED
-          : Characteristic.CurrentDoorState.OPEN
-      );
-      this.service.updateCharacteristic(
-        Characteristic.TargetDoorState,
-        e.state === 'CLOSED'
-          ? Characteristic.CurrentDoorState.CLOSED
-          : Characteristic.CurrentDoorState.OPEN
-      );
+      const isClosed = e.state === 'CLOSED';
+      if (isClosed) {
+        // we control only the close state, since open is done through the timeout above
+        this.service.updateCharacteristic(
+          Characteristic.CurrentDoorState,
+          Characteristic.CurrentDoorState.CLOSED
+        );
+        this.service.updateCharacteristic(
+          Characteristic.TargetDoorState,
+          Characteristic.TargetDoorState.CLOSED
+        );
+      }
     } else {
       this.logger.warn('GarageDoorOpener Service non yet initialized');
     }
   }
 
-  private connectToESPSource() {
+  private connectToESPSource(): Promise<EventSource> {
     return new Promise((resolve, reject) => {
-      this.eventSource = initESPHome(this.config.host, this.config.port, this.logger, e =>
-        this.handleDoorState(e)
-      );
-      this.eventSource.onerror = e => {
-        this.logger.error('Connection error, reinitialize...', e);
+      try {
+        const eventSource = initESPHome(this.config.host, this.config.port, this.logger, state =>
+          this.handleDoorState(state)
+        );
+        eventSource.onerror = async e => {
+          this.logger.error('Connection error, reinitialize...', e);
+          this.closeConnection();
+          await this.initConnection();
+        };
+        eventSource.onopen = m => {
+          if (!this.initialized) {
+            this.logger.info('Connection to ESP initialized...', m);
+            this.initialized = true;
+            resolve(eventSource);
+          }
+        };
+        eventSource.addEventListener('ping', () => {
+          this.aliveTimeout.refresh();
+        });
+        this.aliveTimeout = setTimeout(async () => {
+          this.closeConnection();
+          await this.initConnection();
+        }, 20 * 1000); // if no ping is received in 20 seconds, we reconnect
+      } catch (e) {
         reject(e);
-      };
-      this.eventSource.onopen = m => {
-        this.logger.error('Connection initialized...', m);
-        resolve(m);
-      };
+      }
     });
   }
 }
